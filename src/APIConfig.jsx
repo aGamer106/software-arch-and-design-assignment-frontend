2b3c459ed20432c6de1a163bbf4e15340b780df0
aGamer106 <agamer106@yahoo.com>
2025-11-28 00:43:39 +0000

    added withCredentials: true - this is CRUCIAL - it allows for the data to be sent WITH the cookie session in place

--- src/APIConfig.jsx
+++ src/APIConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Matches your Spring Boot Controller
+    baseURL: 'http://localhost:8080/api',
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     }
