97f6087122a2c791eacb29cef0c4a6ba094b3984
aGamer106 <agamer106@yahoo.com>
2025-11-30 21:02:37 +0000

    added new /profile route

--- src/App.js
+++ src/App.js
@@ -6,23 +6,19 @@ import './static/css/home.css';
 import './static/util/call-centre-pic.jpg';
 import HomeForm from "./components/HomeForm";
 import ConsumerDashboard from "./components/ConsumerDashboard";
-import {BrowserRouter} from "react-router-dom";
 
-// --- THE FIX IS HERE ---
-// You must import 'BrowserRouter' and alias it as 'Router'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ConsumerProfile from "./components/ConsumerProfile";
 
 function App(props) {
     return (
-        // This 'Router' is now actually 'BrowserRouter'
+
         <Router>
             <div>
                 <Navbar/>
                 <Routes>
-                    {/* Route for the Consumer Login (Home) */}
                     <Route path="/" element={<HomeForm />} />
-
-                    {/* Route for the Consumer Dashboard (Protected) */}
+                    <Route path="/profile" element={<ConsumerProfile />} />
                     <Route path="/my-area" element={<ConsumerDashboard />} />
                 </Routes>
                 <Footer/>
