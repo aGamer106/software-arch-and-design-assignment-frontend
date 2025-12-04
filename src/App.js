500f4d915bbc28960c10db5b93cd58c79d02837e
aGamer106 <agamer106@yahoo.com>
2025-12-04 23:45:30 +0000

    new routes added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './static/css/home.css';
 import './static/util/call-centre-pic.jpg';
 import HomeForm from "./components/HomeForm";
 import ConsumerDashboard from "./components/ConsumerDashboard";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ConsumerProfile from "./components/ConsumerProfile";
 import AdminLoginPage from "./components/AdminLoginPage";
+import ManagerDashboard from "./components/ManagerDashboard";
+import AgentDashboard from "./components/AgentDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import SupportPersonDashboard from "./components/SupportPersonDashboard";
 
 function App(props) {
     return (
@@ -22,6 +25,10 @@ function App(props) {
                     <Route path="/profile" element={<ConsumerProfile />} />
                     <Route path="/my-area" element={<ConsumerDashboard />} />
                     <Route path="/admin-login" element={<AdminLoginPage />} />
+                    <Route path="/agent-dashboard" element={<AgentDashboard />} />
+                    <Route path="/support-dashboard" element={<SupportPersonDashboard />} />
+                    <Route path="/manager-dashboard" element={<ManagerDashboard />} />
+                    <Route path="/admin-panel" element={<AdminDashboard />} />
                 </Routes>
                 <Footer/>
             </div>
